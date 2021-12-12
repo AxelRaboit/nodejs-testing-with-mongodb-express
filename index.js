ef5ddb4ec48de17c0f4df6de8c3965f9284e55df
Axel Raboit <axel.raboit@gmail.com>
2021-12-12 16:27:36 +0100

    crud for the api

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express =  require('express');
 const app = express();
 require('./models/dbConfig');
 const postsRoutes = require('./routes/postsController');
-const bodyParser = require('bodyParser');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+/* const mongoose = require('mongoose') */
+//These lines are necessary if there is warning message about Deprecation
+/* mongoose.set('useFindAndModify', false); */
 
 app.use(bodyParser.json());
+app.use(cors());
+/* app.use(cors({origin: 'https://axel-raboit.fr'})); <- To allow a website to access */ 
+//Cors allow everybody to access to the API (We can put params)
 app.use('/posts', postsRoutes);
 //When we are on the '/' we render the postsRoutes
 //who's the postsController file 
--- routes/postsController.js
+++ routes/postsController.js
@@ -1,25 +1,69 @@
 const express = require('express');
 const router = express.Router();
+const ObjectID = require('mongoose').Types.ObjectId;
 
 const { PostsModel } = require('../models/postsModel');
 
-//This route is like localhost:5500/posts'/'
+//GET DATA
 router.get('/', (req, res) => {
-    PostsModel.find((err, docs) => {
-        /* console.log(docs); */
-        if(!err){
-            res.send(docs);
-        } else {
-            console.log('Error to get datas:' + err);
-        }
-    })
+  PostsModel.find((err, docs) => {
+    if (!err) res.send(docs);
+    else console.log("Error to get data : " + err);
+  })
 });
 
+//CREATE DATA
 router.post('/', (req, res) => {
-    const newRecord = new PostsModel({
-        author: req.body.author,
-        message: req.body.message
-    });
+  const newRecord = new PostsModel({
+    author: req.body.author,
+    message: req.body.message
+  });
+
+  newRecord.save((err, docs) => {
+    if (!err) res.send(docs);
+    else console.log('Error creating new data : ' + err);
+  })
+});
+
+// UPDATE DATA
+router.put("/:id", (req, res) => {
+  if (!ObjectID.isValid(req.params.id)) {
+      return res.status(400).send("ID unknow : " + req.params.id)
+  }
+  
+  const updateRecord = {
+    author: req.body.author,
+    message: req.body.message
+  };
+
+  PostsModel.findByIdAndUpdate(
+    req.params.id,
+    { $set: updateRecord},
+    { new: true },
+    (err, docs) => {
+      if (!err) res.send(docs);
+      else console.log("Update error : " + err);
+    }
+  )
+});
+
+// DELETE DATA
+router.delete("/:id",(req, res) => {
+    if (!ObjectID.isValid(req.params.id)) {
+        return res.status(400).send("ID unknow : " + req.params.id)
+    }
+
+    PostsModel.findByIdAndRemove(
+        req.params.id,
+        (err, docs) => { 
+            if(!err) {
+                res.send(docs)
+            } else {
+                console.log('Delete error: ' + err);
+            }
+            
+        }
+    )
 })
 
 module.exports = router;
