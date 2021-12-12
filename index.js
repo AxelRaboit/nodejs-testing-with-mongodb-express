988d93056a676c155768b9086b2b62d3fce92b4f
Axel Raboit <axel.raboit@gmail.com>
2021-12-12 14:11:27 +0100

    complete the connexion to get datas from the mongodb database

--- index.js
+++ index.js
@@ -2,4 +2,11 @@ const express =  require('express');
 const app = express();
 require('./models/dbConfig');
 
+const postsRoutes = require('./routes/postsController');
+
+app.use('/', postsRoutes);
+//When we are on the '/' we render the postsRoutes
+//who's the postsController file 
+
+
 app.listen(5500, () => console.log('Server started: 5500'));
